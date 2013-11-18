fb99411dfe19d87866e902e2de5febe867266427
David Alméciga <wdavid@dagrinchi.com>
2013-11-17 23:56:00 -0500

    Share plugin

--- js/app/views/detalleProyecto.js
+++ js/app/views/detalleProyecto.js
@@ -23,6 +23,29 @@ define(function(require) {
 
 		template: _.template(detalleProyectoTpl),
 
+		events: {
+			"click .share": "btnShare"
+		},
+
+		btnShare: function() {
+			window.plugins.socialsharing.available(function(isAvailable) {
+				if (isAvailable) {
+					window.plugins.socialsharing.share("APC-mapps", "APC-mapps");
+				}
+			});
+			// require(['html2canvas'], function() {
+			// 	html2canvas(document.getElementsByTagName("body"), {
+			// 		onrendered: function(canvas) {
+			// 			window.plugins.socialsharing.available(function(isAvailable) {
+			// 				if (isAvailable) {
+			// 					window.plugins.socialsharing.share(null, "APC-mapps", canvas.toDataURL(), null);
+			// 				}
+			// 			});
+			// 		}
+			// 	});
+			// });			
+		},
+
 		render: function() {
 			this.$el.html(this.template(this.model.toJSON()));
 			return this;
